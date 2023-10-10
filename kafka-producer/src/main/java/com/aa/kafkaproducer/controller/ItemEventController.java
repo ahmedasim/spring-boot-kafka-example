c192776f6daef1a5379996f0a3a612fccbe7d401
Asim Ahmed <Asim Ahmed@AsimAhmed-PC.mshome.net>
2023-10-10 12:31:24 +0500

    kafka producer

---
+++ kafka-producer/src/main/java/com/aa/kafkaproducer/controller/ItemEventController.java
@@ -0,0 +1,41 @@
+package com.aa.kafkaproducer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aa.kafkaproducer.dto.InventoryItemDto;
+import com.aa.kafkaproducer.service.ItemEventPublisher;
+
+@RestController
+@RequestMapping("/producer")
+public class ItemEventController {
+
+	@Autowired
+	private ItemEventPublisher publisher;
+	
+	
+	@PostMapping("/publish/{msg}")
+    public ResponseEntity<?> publishMessage(@PathVariable String msg) {
+        try {
+            for (int i = 0; i <= 100000; i++) {
+                publisher.sendMessageToTopic(msg + " -> " + i);
+            }
+            return ResponseEntity.ok("message published successfully ..");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+	
+	@PostMapping("/publish")
+    public void sendEvents(@RequestBody InventoryItemDto dto) {
+        publisher.sendEventsToTopic(dto);
+    }
+
+}
